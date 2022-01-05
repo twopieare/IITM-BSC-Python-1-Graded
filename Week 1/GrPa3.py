4bdcda4dce3da87902e8eaa170f73492a2fce20c
Shivam Kushwaha <91305830+shivam-kush@users.noreply.github.com>
2022-01-05 20:13:37 +0530

    created grpa3.py

---
+++ Week 1/GrPa3.py
@@ -0,0 +1,4 @@
+#Accept a sequence of five single digit numbers separated by commas as input. Print the product of all five numbers.
+
+inp = input()
+print(int(inp[0])*int(inp[2])*int(inp[4])*int(inp[6])*int(inp[8]))
