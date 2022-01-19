08408c4fb35089a848c4aff16488166287a371dc
2pieare <91305830+shivam-kush@users.noreply.github.com>
2022-01-19 23:45:53 +0530

    corrected consecutive cond

--- Week 3/GrPa5.py
+++ Week 3/GrPa5.py
@@ -1,14 +1,31 @@
-contact = input()
-together = 0
-totalcount = 0
-flag = 1
-if((contact[0]=='6' or contact[0]=='7' or contact[0]=='8' or contact[0]=='9') and len(contact)==10):
-    for i in range(1,10):
-        if(contact[i-1]==contact[i]):
-            together += 1
-            if(together>5):
-                flag = 0
-if flag==1:
-    print("valid")
-else:
-    print("invalid")
+def isValid(ph):
+    li=[]
+    while ph>0:
+        li=[ph%10]+li
+        ph=ph//10
+    n=len(li)
+    set1={6,7,8,9}
+    if n!=10 or li[0] not in set1:
+        print('invalid')
+        return
+    li1=[0 for i in range(10)] # li1[i] is the frequency of i
+    for i in li:
+        li1[i]+=1
+    for j in li1:
+        if j>=7:
+            print('invalid')
+            return
+    i,count=0,1             # the 5 consecutive element cond.,linear search
+    while i<n-1:
+        if li[i+1]==li[i]:
+            count+=1
+            if count>5:
+                print('invalid')
+                return
+        else:
+            count=1
+        i+=1
+    print('valid')
+    return
+ph = int(input())
+isValid(ph)
